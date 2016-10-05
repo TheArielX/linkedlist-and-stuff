0f736ce95d352043b4c64ecd7fb739b9fa25f648
TheArielX <arieltiburcio@gmail.com>
2016-10-05 11:16:29 -0400

    Create PilaLista.h

---
+++ PilaLista.h
@@ -0,0 +1,14 @@
+#ifndef PILA_H_INCLUDED
+#define PILA_H_INCLUDED
+
+#include "LinkedListC.h"
+
+typedef struct
+{
+  Lista datos;
+} PilaLista;
+
+void push(PilaLista &p, int value);
+int pop(PilaLista &p);
+int peek(PilaLista p);
+bool isEmpty(PilaLista p);
